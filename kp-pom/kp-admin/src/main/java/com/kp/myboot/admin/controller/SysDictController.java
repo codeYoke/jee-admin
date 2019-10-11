5292e61b7e265d6d7bab6d43055898260acc91ee
fjh <920766616@qq.com>
2019-10-11 18:29:32 +0800

    m第一次提交，基本业务接口已实现

---
+++ kp-pom/kp-admin/src/main/java/com/kp/myboot/admin/controller/SysDictController.java
@@ -0,0 +1,47 @@
+package com.kp.myboot.admin.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kp.myboot.admin.model.SysDict;
+import com.kp.myboot.admin.service.SysDictService;
+import com.kp.myboot.core.http.HttpResult;
+import com.kp.myboot.core.page.PageRequest;
+
+/**
+ * 字典控制器
+ */
+@RestController
+@RequestMapping("dict")
+public class SysDictController {
+
+    @Autowired
+    private SysDictService sysDictService;
+
+    @PostMapping(value = "/save")
+    public HttpResult save(@RequestBody SysDict record) {
+        return HttpResult.ok(sysDictService.save(record));
+    }
+
+    @PostMapping(value = "/delete")
+    public HttpResult delete(@RequestBody List<SysDict> records) {
+        return HttpResult.ok(sysDictService.delete(records));
+    }
+
+    @PostMapping(value = "/findPage")
+    public HttpResult findPage(@RequestBody PageRequest pageRequest) {
+        return HttpResult.ok(sysDictService.findPage(pageRequest));
+    }
+
+    @GetMapping(value = "/findByLable")
+    public HttpResult findByLable(@RequestParam String lable) {
+        return HttpResult.ok(sysDictService.findByLable(lable));
+    }
+}
---
+++ kp-pom/kp-admin/src/main/java/com/kp/myboot/admin/service/SysDeptService.java
@@ -0,0 +1,19 @@
+package com.kp.myboot.admin.service;
+
+import java.util.List;
+
+import com.kp.myboot.admin.model.SysDept;
+import com.kp.myboot.core.service.CurdService;
+
+/**
+ * 机构管理
+ */
+public interface SysDeptService extends CurdService<SysDept> {
+
+    /**
+     * 查询机构树
+     *
+     * @return
+     */
+    List<SysDept> findTree();
+}
