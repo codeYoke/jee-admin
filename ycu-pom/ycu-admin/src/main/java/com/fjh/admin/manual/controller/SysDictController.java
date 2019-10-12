b77dcbe5ea3c20d4eeb30da2b39b23ac650f99d1
fjh <920766616@qq.com>
2019-10-12 21:59:08 +0800

    字典控制器接口添加了spring security安全验证

--- ycu-pom/ycu-admin/src/main/java/com/fjh/admin/manual/controller/SysDictController.java
+++ ycu-pom/ycu-admin/src/main/java/com/fjh/admin/manual/controller/SysDictController.java
@@ -6,6 +6,7 @@ import com.fjh.admin.manual.service.SysDictService;
 import com.fjh.core.http.HttpResult;
 import com.fjh.core.page.PageRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,6 +26,8 @@ public class SysDictController {
      * @param record
      * @return
      */
+
+    @PreAuthorize("hasAuthority('sys:dict:add') and hasAuthority('sys:dict:edit')")
     @PostMapping(value = "/save")
     public HttpResult save(@RequestBody SysDict record) {
         return HttpResult.ok(sysDictService.save(record));
@@ -35,6 +38,7 @@ public class SysDictController {
      * @param records
      * @return
      */
+    @PreAuthorize("hasAuthority('sys:dict:delete')")
     @PostMapping(value = "/delete")
     public HttpResult delete(@RequestBody List<SysDict> records) {
         return HttpResult.ok(sysDictService.delete(records));
@@ -45,11 +49,12 @@ public class SysDictController {
      * @param pageRequest
      * @return
      */
+    @PreAuthorize("hasAuthority('sys:dict:view')")
     @PostMapping(value = "/findPage")
     public HttpResult findPage(@RequestBody PageRequest pageRequest) {
         return HttpResult.ok(sysDictService.findPage(pageRequest));
     }
-
+    @PreAuthorize("hasAuthority('sys:dict:view')")
     @GetMapping(value = "/findByLable")
     public HttpResult findByLable(@RequestParam String lable) {
         return HttpResult.ok(sysDictService.findByLable(lable));
