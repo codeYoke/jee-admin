0d158e3e106973d136e5246585c35bd20f267540
fjh <920766616@qq.com>
2019-10-13 12:02:27 +0800

    各个模块添加Consul实现分布式系统服务的发现，注冊和配置

--- ycu-monitor/src/main/java/com/fjh/monitor/YcuMonitorApplication.java
+++ ycu-monitor/src/main/java/com/fjh/monitor/YcuMonitorApplication.java
@@ -3,8 +3,10 @@ package com.fjh.monitor;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableAdminServer //监听服务
+@EnableDiscoveryClient//开启服务发现支持
 @SpringBootApplication
 public class YcuMonitorApplication {
 
